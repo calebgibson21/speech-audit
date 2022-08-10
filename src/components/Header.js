aeb7d99092d9b9b8b7a1cf21a014892f46282ea1
Caleb Gibson <calebgibson21@gmail.com>
2022-08-10 16:31:28 -0500

    added flashcard logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
-import React from 'react'
 import '../App.css';
+import logo from '../images/flash-cards 1.png';
 
 const Header = () => {
   return (<>
   <div className='header'>
       <div className='container'>
           <div className='header-row'>
-              <h1>PM Flashcards</h1>
-              <h1>Other Copy</h1>
+            <div className='header-logo'>
+              <h1 style={{color: "#1161D4"}}>PM Flashcards</h1>
+              <img src={logo} alt='flashcard-logo' style={{height: "100%"}}/>
             </div>
+            <h1>Other Copy</h1>
+          </div>
       </div>
   </div>
     </>
